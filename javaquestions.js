1156c72bb2a991b1c4e67c84ce0ee1863c9e8352
NathanP1234 <111771946+NathanP1234@users.noreply.github.com>
2023-01-25 08:46:33 -0700

    Add files via upload

--- javaquestions.js
+++ javaquestions.js
@@ -1,27 +1,85 @@
-var questions = [
+var myQuestions = [
   {
-    title: "Commonly used data types DO NOT include:",
-    choices: ["strings", "booleans", "alerts", "numbers"],
-    answer: "alerts"
+    question: "Commonly used data types DO NOT include:",
+    answers: ["strings", "booleans", "alerts", "numbers"],
+    correctAnswer: "alerts"
   },
   {
-    title: "The condition in an if / else statement is enclosed within:",
-    choices: ["quotes", "curly brackets", "parentheses", "square brackets"],
-    answer: "parentheses"
+    question: "The condition in an if / else statement is enclosed within:",
+    answers: ["quotes", "curly brackets", "parentheses", "square brackets"],
+    correctAnswer: "parentheses"
   },
   {
-    title: "Arrays in JavaScript can be used to store:",
-    choices: ["numbers and strings", "other arrays", "booleans", "all of the above"],
-    answer: "all of the above"
+    question: "Arrays in JavaScript can be used to store:",
+    answers: ["numbers and strings", "other arrays", "booleans", "all of the above"],
+    correctAnswer: "all of the above"
   },
   {
-    title: "String values, when being assigned to variables, must be enclosed within:",
-    choices: ["commas", "curly brackets", "quotes", "parentheses"],
-    answer: "quotes"
+    question: "String values, when being assigned to variables, must be enclosed within:",
+    answers: ["commas", "curly brackets", "quotes", "parentheses"],
+    correctAnswer: "quotes"
   },
   {
-    title: 'A very useful tool used during development and debugging for printing content to the debugger is:',
-    choices: ["JavaScript", "terminal / bash", "for loops", "console.log"],
-    answer: "console.log"
+    question: 'A very useful tool used during development and debugging for printing content to the debugger is:',
+    answers: ["JavaScript", "terminal / bash", "for loops", "console.log"],
+    correctAnswer: "console.log"
   }
 ];
+
+function showQuestions(questions, quizContainer){
+	var output = [];
+	var answers;
+
+	for(var i=0; i<questions.length; i++){
+		
+		answers = [];
+
+		for(letter in questions[i].answers){
+
+			answers.push(
+				'<label>'
+					+ '<input type="radio" name="question'+i+'" value="'+letter+'">'
+					+ letter + ': '
+					+ questions[i].answers[letter]
+				+ '</label>'
+			);
+		}
+
+		output.push(
+			'<div class="question">' + questions[i].question + '</div>'
+			+ '<div class="answers">' + answers.join('') + '</div>'
+		);
+	}
+
+	quizContainer.innerHTML = output.join('');
+}
+
+showQuestions(questions, quizContainer);
+
+function showResults(questions, quizContainer, resultsContainer){
+	
+	var answerContainers = quizContainer.querySelectorAll('.answers');
+	
+	var userAnswer = '';
+	var numCorrect = 0;
+	
+	for(var i=0; i<questions.length; i++){
+
+		userAnswer = (answerContainers[i].querySelector('input[name=question'+i+']:checked')||{}).value;
+		
+		if(userAnswer===questions[i].correctAnswer){
+			numCorrect++;
+			
+			answerContainers[i].style.color = 'lightgreen';
+		}
+		else{
+			answerContainers[i].style.color = 'red';
+		}
+	}
+
+	resultsContainer.innerHTML = numCorrect + ' out of ' + questions.length;
+}
+
+submitButton.onclick = function(){
+	showResults(questions, quizContainer, resultsContainer);
+}
